2cba687866143b2ac9459f80fac86b36e32363ec
mbasim25 <mohammedbasim2525@gmail.com>
2021-05-22 15:51:47 +0300

    registeration

--- src/controllers/accounts.ts
+++ src/controllers/accounts.ts
@@ -2,11 +2,25 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 
 import * as validators from "../utils/validators";
-import { User } from "../types/users";
+import { Account } from "../types/accounts";
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-class Controller {}
+class Controller {
+  registration = async (req: Request, res: Response) => {
+    try {
+      const data: Account = await validators.register.validateAsync(req.body);
+      data.password = await bcrypt.hash(data.password, 12);
+      const account = await prisma.account.create({
+        data: {},
+      });
+
+      return res.status(201).json();
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  };
+}
 
 export default new Controller();
