dd5bcf5f890a875bbc7d0ebfd9f489dbf9bc9f7c
mbasim25 <mohammedbasim2525@gmail.com>
2021-06-12 03:21:31 +0300

    extra

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import request from "supertest";
-import app from "../src/app";
-
-describe("Test / route", () => {
-  test("Get request should returns 200 status code", async () => {
-    await request(app).get("/").expect("Content-Type", /json/).expect(200);
-  });
-});
