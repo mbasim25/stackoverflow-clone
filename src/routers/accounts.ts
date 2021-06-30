f4ba34365476eb23fc40a7b0ae8502f468e84114
mbasim25 <mohammedbasim2525@gmail.com>
2021-06-30 23:26:23 +0300

    added: router level jwt

--- src/routers/accounts.ts
+++ src/routers/accounts.ts
@@ -14,35 +14,25 @@ router.post("/login", controller.login);
 // Refresh token
 router.post("/refresh-token", controller.refreshToken);
 
-// Get Profile
-router.get(
-  "/profile",
-  passport.authenticate("jwt", { session: false }),
-  controller.profile
-);
-
-// Super admin for development only
-router.post("/super", controller.super);
-
-// Update password when authenticated
-router.patch(
-  "/passwords/change",
-  passport.authenticate("jwt", { session: false }),
-  controller.passwordChange
-);
-
-// Update profile
-router.patch(
-  "/profile",
-  passport.authenticate("jwt", { session: false }),
-  uploads.userImage,
-  controller.updateAccount
-);
-
 // Send email for password reset when non authenticated
 router.post("passwords/email", controller.resetEmail);
 
 // Update password when non authenticated
 router.patch("/passwords/reset", controller.resetConfirm);
+
+// Super admin for development only
+router.post("/super", controller.super);
+
+// JWT required for all the enpoints below
+router.use(passport.authenticate("jwt", { session: false }));
+
+// Get Profile
+router.get("/profile", controller.profile);
+
+// Update password when authenticated
+router.patch("/passwords/change", controller.passwordChange);
+
+// Update profile
+router.patch("/profile", uploads.userImage, controller.updateAccount);
 
 export default router;
