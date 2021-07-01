ae1d9ace09bc1611ebff7c7128f1925151ca3c1a
mbasim25 <mohammedbasim2525@gmail.com>
2021-07-01 23:02:57 +0300

    monor changes to user router

--- src/routers/accounts.ts
+++ src/routers/accounts.ts
@@ -5,19 +5,15 @@ import { uploads } from "../utils";
 
 const router = Router();
 
-// Registeration
+// Registeration & Login
 router.post("/register", uploads.userImage, controller.registration);
-
-// Login
 router.post("/login", controller.login);
 
 // Refresh token
 router.post("/refresh-token", controller.refreshToken);
 
-// Send email for password reset when non authenticated
+// Password reset
 router.post("passwords/email", controller.resetEmail);
-
-// Update password when non authenticated
 router.patch("/passwords/reset", controller.resetConfirm);
 
 // Super admin for development only
@@ -26,13 +22,11 @@ router.post("/super", controller.super);
 // JWT required for all the enpoints below
 router.use(passport.authenticate("jwt", { session: false }));
 
-// Get Profile
+// Get & Update Profile
 router.get("/profile", controller.profile);
+router.patch("/profile", uploads.userImage, controller.updateAccount);
 
 // Update password when authenticated
 router.patch("/passwords/change", controller.passwordChange);
-
-// Update profile
-router.patch("/profile", uploads.userImage, controller.updateAccount);
 
 export default router;
