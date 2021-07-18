b2775a58a25ca064e127bb5605c629283cbe570c
mbasim25 <mohammedbasim2525@gmail.com>
2021-07-18 23:37:31 +0300

    Created: prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { users, fields } from "./data";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  try {
+    // Create fields
+    await fields.fieldCreate(prisma);
+
+    // Create superadmin
+    await users.superCreate(prisma);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+main()
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
