af6d979c064f21c094913e9454cb286b169f6d92
mbasim25 <mohammedbasim2525@gmail.com>
2021-08-14 02:28:11 +0300

    Added: list middleware to answer list router

--- src/routers/answers.ts
+++ src/routers/answers.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import passport from "passport";
 import controller from "../controllers/answers";
-import { isOwnerOrAdmin } from "../middlewares/permissions";
+import { isOwnerOrAdmin, listMiddleWare } from "../middlewares";
 
 const router = Router();
 
 // List
-router.get("/", controller.list);
+router.get("/", listMiddleWare, controller.list);
 
 // JWT Auth
 router.use(passport.authenticate("jwt", { session: false }));
