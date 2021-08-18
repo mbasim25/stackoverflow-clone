f8d7ff70e8dd211ce17b294d968794b25f2244cc
mbasim25 <mohammedbasim2525@gmail.com>
2021-08-18 20:07:22 +0300

    Added: find user by id endpoint

--- src/routers/accounts.ts
+++ src/routers/accounts.ts
@@ -16,6 +16,9 @@ router.post("/tokens/refresh", controller.refreshToken);
 router.post("/passwords/email", controller.resetEmail);
 router.patch("/passwords/reset", controller.resetConfirm);
 
+// Find a user by id
+router.get("/user/:id", controller.retrieve);
+
 // JWT required for all the endpoints below
 router.use(passport.authenticate("jwt", { session: false }));
 
